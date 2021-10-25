0788bc3b1b2b8cf2810d23e8747d6382e7195c1b
mfchavesjr <feira.nova@gmail.com>
2021-10-25 15:24:36 -0300

    first commit

---
+++ src/main/java/com/mchaves/Suporte/a/Clientes/SuporteAClientesApplication.java
@@ -0,0 +1,13 @@
+package com.mchaves.Suporte.a.Clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuporteAClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuporteAClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mchaves/Suporte/a/Clientes/SuporteAClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchaves.Suporte.a.Clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteAClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
